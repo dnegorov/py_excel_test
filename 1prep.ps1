1e540b152eba2ccfe2f29b6a588b6522e12d2791
Denis Egorov <55288899+dnegorov@users.noreply.github.com>
2021-01-16 17:28:42 +0300

    Create 1prep.ps1

---
+++ 1prep.ps1
@@ -0,0 +1,23 @@
+﻿# Get path of project i.e. c:\dev\project1
+$project_path = $PWD.Path
+
+# Defaul python environment folder i.e. c:\dev\project1\.env
+$pyenv_folder_name = ".env"
+$project_py_path = $project_path + "\"+ $pyenv_folder_name
+
+# Prompt in new environment
+$pyenv_prompt = "EXCEL"
+
+# Create python virtual environment with venv
+& python -m venv .\$pyenv_folder_name --prompt $pyenv_prompt
+
+# Activate project virtual environment
+& $project_py_path"\scripts\activate.ps1"
+
+# Update python tools & pip
+python -m pip install --upgrade setuptools
+python -m pip install --upgrade pip
+
+# install project requirements
+# pip install -r dev-requirements.txt
+pip install -r requirements.txt
